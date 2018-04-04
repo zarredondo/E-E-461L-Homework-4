b1e4b21bc2f18838c1c1f7ec1a12d2182ce68696
Brandon <brandonbui50@yahoo.com>
2018-04-04 14:38:54 -0500

    MapActivity in progress

--- app/src/main/java/vn/pugs/gocoder/Location.java
+++ app/src/main/java/vn/pugs/gocoder/Location.java
@@ -1,17 +1,20 @@
 package vn.pugs.gocoder;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import com.google.android.maps.GeoPoint;
+import com.google.android.maps.MapActivity;
+import com.google.android.maps.MapController;
+import com.google.android.maps.MapView;
+import com.google.android.maps.Overlay;
 
 /**
  * Created by Brandon on 4/3/2018.
  */
 
-public class Location extends AppCompatActivity {
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        //TODO find Map location on map
+public class Location extends MapActivity {
 
-    }
+    private MapView mapView;
+    GeoPoint geoPoint;
 }
